76dad05ffefa9283cc0bdb67cda4f61f48a6501f
kareem iyabo <kareemomowumi60@gmail.com>
2023-03-17 04:34:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
+/**
  * main - A program that prints the site of various computer types
  * Return 0 (Success)
  */
-int main(void)
-{
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float f;
 int main(void) {
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
 printf("Size of an int: %ld byte(s)\n", sizeof(int));
@@ -17,4 +11,3 @@ printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
   return 0;
 }
-
